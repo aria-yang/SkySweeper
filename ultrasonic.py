cf08052cd8c64fc05b12f472a5691f8de7a64beb
aria-yang <aria.yang@mail.utoronto.ca>
2025-03-17 19:20:11 -0400

    Fixed formatting issues for ultrasonic.py

--- ultrasonic.py
+++ ultrasonic.py
@@ -1,17 +1,26 @@
+'''
+ESC204 Lab 3, Task G
+Task: Calibrate an ultrasonic sensor.
+'''
+
 import time
 import board
-import adafruit hcsr04
+import adafruit_hcsr04
+
 # Set up the ultrasonic sensor using a library
-sonar = adafruit hcsr04.HCSR04(trigger pin=board.GP2, echo pin=board.
-GP3)
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.GP2, echo_pin=board.GP3)
+
 # Take readings and output calibrated values
 while True:
-try:
-# Take a reading (no button needed) of the range
-X = sonar.distance
-# Use calibration data to adjust this value
-real dist = 1.084∗(X−9.044)+10
-print((real dist ,))
-except RuntimeError:
-print("Retrying!")
-time.sleep(2)
+    try:
+        # Take a reading (no button needed) of the range
+        X = sonar.distance
+
+        # Use calibration data to adjust this value
+        real_dist = 1.084*(X-9.044)+10
+        print((real_dist,))
+
+    except RuntimeError:
+        print("Retrying!")
+
+    time.sleep(2)

6edd771c579501bf2cf93a4d9e06772586cc572d
aria-yang <aria.yang@mail.utoronto.ca>
2025-03-29 23:00:57 -0400

    Test code works- 110 degrees equals 180 for some reason

--- test.py
+++ test.py
@@ -44,7 +44,7 @@ ena2 = pwmio.PWMOut(board.GP2, duty_cycle = 0)
 # enb2 = pwmio.PWMOut(board.GP21, duty_cycle = 0)
 
 # create a PWMOut object for servomotor
-pwm = pwmio.PWMOut(board.GP4, duty_cycle=2 ** 15, frequency=50)
+pwm = pwmio.PWMOut(board.GP4, duty_cycle=0, frequency=50)
 
 # Create a servo object, my_servo.
 my_servo = servo.Servo(pwm)
@@ -178,10 +178,5 @@ while True:
             time.sleep(2)
 
             # servo test
-            print("Servo shaft is turning now.")
-            for angle in range(0, 60, 5):  # 0 - 180 degrees, 5 degrees at a time.
-                my_servo.angle = angle
-                time.sleep(0.05)
-            for angle in range(60, 0, -5):  # 180 - 0 degrees, 5 degrees at a time.
-                my_servo.angle = angle
-                time.sleep(0.05)
+            my_servo.angle = 110
+            
